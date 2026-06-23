6ecb98e3170867b54dd1b3c60adb57f4621711a6
Francis Jacobs <fransuiss1016@gmail.com>
2026-06-23 18:00:19 +0200

    solar module change

--- ModulePVLIB_7daypred.py
+++ ModulePVLIB_7daypred.py
@@ -97,107 +97,113 @@ print(weather)
 
 
 #%%
-system = {'module': module, 'inverter': inverter,
+def getACPower():
+    system = {'module': module, 'inverter': inverter,
+    
+              'surface_azimuth': 180}
+    
+    latitude, longitude, name, altitude, timezone = location
+    
+    system['surface_tilt'] = latitude
+    
+    solpos = pvlib.solarposition.get_solarposition(
+    
+        time=weather.index,
+    
+        latitude=latitude,
+    
+        longitude=longitude,
+    
+        altitude=altitude,
+    
+        temperature=weather["temperature_2m"],
+    
+        pressure=weather["surface_pressure"],
+    
+    )
+    
+    dni_extra = pvlib.irradiance.get_extra_radiation(weather.index)
+    
+    airmass = pvlib.atmosphere.get_relative_airmass(solpos['apparent_zenith'])
+    
+    pressure = pvlib.atmosphere.alt2pres(altitude)
+    
+    am_abs = pvlib.atmosphere.get_absolute_airmass(airmass, pressure)
+    
+    aoi = pvlib.irradiance.aoi(
+    
+        system['surface_tilt'],
+    
+        system['surface_azimuth'],
+    
+        solpos["apparent_zenith"],
+    
+        solpos["azimuth"],
+    
+    )
+    
+    total_irradiance = pvlib.irradiance.get_total_irradiance(
+    
+        system['surface_tilt'],
+    
+        system['surface_azimuth'],
+    
+        solpos['apparent_zenith'],
+    
+        solpos['azimuth'],
+    
+        weather['direct_normal_irradiance'],
+    
+        weather['shortwave_radiation'],
+    
+        weather['diffuse_radiation'],
+    
+        dni_extra=dni_extra,
+    
+        model='haydavies',
+    
+    )
+    
+    cell_temperature = pvlib.temperature.sapm_cell(
+    
+        total_irradiance['poa_global'],
+    
+        weather["temperature_2m"],
+    
+        weather["wind_speed_10m"],
+    
+        **temperature_model_parameters,
+    
+    )
+    
+    effective_irradiance = pvlib.pvsystem.sapm_effective_irradiance(
+    
+        total_irradiance['poa_direct'],
+    
+        total_irradiance['poa_diffuse'],
+    
+        am_abs,
+    
+        aoi,
+    
+        module,
+    
+    )
+    
+    dc = pvlib.pvsystem.sapm(effective_irradiance, cell_temperature, module)
+    
+    ac = pvlib.inverter.sandia(dc['v_mp'], dc['p_mp'], inverter)
+    return ac
 
-          'surface_azimuth': 180}
 
-latitude, longitude, name, altitude, timezone = location
-
-system['surface_tilt'] = latitude
-
-solpos = pvlib.solarposition.get_solarposition(
-
-    time=weather.index,
-
-    latitude=latitude,
-
-    longitude=longitude,
-
-    altitude=altitude,
-
-    temperature=weather["temperature_2m"],
-
-    pressure=weather["surface_pressure"],
-
-)
-
-dni_extra = pvlib.irradiance.get_extra_radiation(weather.index)
-
-airmass = pvlib.atmosphere.get_relative_airmass(solpos['apparent_zenith'])
-
-pressure = pvlib.atmosphere.alt2pres(altitude)
-
-am_abs = pvlib.atmosphere.get_absolute_airmass(airmass, pressure)
-
-aoi = pvlib.irradiance.aoi(
-
-    system['surface_tilt'],
-
-    system['surface_azimuth'],
-
-    solpos["apparent_zenith"],
-
-    solpos["azimuth"],
-
-)
-
-total_irradiance = pvlib.irradiance.get_total_irradiance(
-
-    system['surface_tilt'],
-
-    system['surface_azimuth'],
-
-    solpos['apparent_zenith'],
-
-    solpos['azimuth'],
-
-    weather['direct_normal_irradiance'],
-
-    weather['shortwave_radiation'],
-
-    weather['diffuse_radiation'],
-
-    dni_extra=dni_extra,
-
-    model='haydavies',
-
-)
-
-cell_temperature = pvlib.temperature.sapm_cell(
-
-    total_irradiance['poa_global'],
-
-    weather["temperature_2m"],
-
-    weather["wind_speed_10m"],
-
-    **temperature_model_parameters,
-
-)
-
-effective_irradiance = pvlib.pvsystem.sapm_effective_irradiance(
-
-    total_irradiance['poa_direct'],
-
-    total_irradiance['poa_diffuse'],
-
-    am_abs,
-
-    aoi,
-
-    module,
-
-)
-
-dc = pvlib.pvsystem.sapm(effective_irradiance, cell_temperature, module)
-
-ac = pvlib.inverter.sandia(dc['v_mp'], dc['p_mp'], inverter)
-print(ac)
-# plt.plot(np.arange(len(ac)), ac)
-ac.plot()
-plt.xlabel("Time in Days")
-plt.ylabel('Hourly power output (W)')
-
-plt.show()
+def run_example():
+    ac = getACPower()
+    print(ac)
+    # plt.plot(np.arange(len(ac)), ac)
+    ac.plot()
+    plt.xlabel("Time in Days")
+    plt.ylabel('Hourly power output (W)')
+    
+    plt.show()
 
 #%%
--- ModuleTotal.py
+++ ModuleTotal.py
@@ -5,4 +5,5 @@ Renewable Energy Production
 import ModulePVLIB_7daypred as pv
 import ModuleWindPowLib_7daypred as wp
 
+pv.run_example()
 wp.run_example()
