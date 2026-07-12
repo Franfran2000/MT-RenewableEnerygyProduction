602970c61c0d07885aa45d47cb54787bf8fa3e31
Francis Jacobs <fransuiss1016@gmail.com>
2026-07-12 17:20:28 +0200

    removed system dict to use module and inverter variables directly

--- pv_module.py
+++ pv_module.py
@@ -20,36 +20,28 @@ class PVModule(RenewModule):
         self.location = Location(**location)
 
     def read_params(self, module_names, inverter_names, surface_azimuth, temperature_models):
-        #TODO allow modules and inverter to be directly given as argument
         sandia_modules = pvlib.pvsystem.retrieve_sam('SandiaMod')
         sapm_inverters = pvlib.pvsystem.retrieve_sam('cecinverter')
         
-        self.system = {}
-        
-        self.system["module"] = sandia_modules[module_names]
-        self.system["inverter"] = sapm_inverters[inverter_names]
-        self.system["surface_azimuth"] = surface_azimuth
+        self.module = sandia_modules[module_names]
+        self.inverter = sapm_inverters[inverter_names]
+        self.surface_azimuth = surface_azimuth
         
         temp_mod_name = temperature_models[0]
         temp_mod_config = temperature_models[1]
         self.temperature_model_parameters = pvlib.temperature.TEMPERATURE_MODEL_PARAMETERS[temp_mod_name][temp_mod_config]    
         
-        module = self.system["module"]
-        inverter = self.system["inverter"]
-        
-        print(self.location.altitude)
-                
+                        
         mount = FixedMount(surface_tilt=self.location.latitude, surface_azimuth=180)
         array = Array(
             mount=mount,
-            module_parameters=module,
+            module_parameters=self.module,
             temperature_model_parameters=self.temperature_model_parameters,
         )
-        pvsystem = PVSystem(arrays=[array], inverter_parameters=inverter)
+        pvsystem = PVSystem(arrays=[array], inverter_parameters=self.inverter)
         self.mc = ModelChain(pvsystem, self.location)
         
     def calculate_power(self, weather):
         self.mc.run_model(weather)
         self.power = self.mc.results.ac
-        print(self.power)
         
